a77d717164d6ef088696a39f0db3268f68086640
Sanae <sanaedariouche@gmail.com>
2018-11-12 17:30:22 +0000

    Quadrant Selection https://open.kattis.com/problems/quadrant

---
+++ Quadrant_Selection.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class K2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x>0 && y>0)
+            System.out.println("1");
+        else if(x<0 && y>0)
+            System.out.println("2");
+        else if(x<0 && y<0)
+            System.out.println("3");
+        else 
+            System.out.println("4");
+        
+    }
+
+}
