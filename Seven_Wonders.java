48c7e974d1c0a8ee6002f7b60277a9c4a85b2cd7
Sanae <sanaedariouche@gmail.com>
2018-11-12 17:31:16 +0000

    Seven Wonders https://open.kattis.com/problems/secretmessage

---
+++ Seven_Wonders.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Seven_Wonders {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc= new Scanner(System.in);
+        String s=sc.nextLine();
+        int nT=0,nC=0,nG=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='T') nT++;
+            else if(s.charAt(i)=='C') nC++;
+            else if(s.charAt(i)=='G') nG++;
+        }
+        int n=Math.min(nT, nC);
+        int sum=nT*nT+nC*nC+nG*nG+Math.min(n, nG)*7;
+        System.out.println(sum);
+    }
+
+}
